5c2afb5bf641cd70d172c1d0faa618c5a267bb0f
berriestrawberrie <brooke.henriksson@outlook.com>
2025-12-02 12:17:48 +0100

    Added dist folder to linting exlcusion for frontend and matched CI node versions for frontend and backend

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    ignores: ["dist/**"],
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
