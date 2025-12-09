7401ab613b7da762448b48d40eece920a354f234
berriestrawberrie <brooke.henriksson@outlook.com>
2025-12-09 12:37:35 +0100

    Added data to profile page

--- frontend/src/routes/Player.tsx
+++ frontend/src/routes/Player.tsx
@@ -1,26 +1,51 @@
-import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import { useAuthStore } from "../store/authStore";
-
-type PlayerParams = {
-  uid: string;
-};
+import { getUserScores } from "../api/playerHandler";
+import type { UserScoreInterface } from "../interfaces/interfaces";
 
 export const Player: React.FC = () => {
-  const { uid } = useParams<PlayerParams>();
   //const [prismaUser, setPrismaUser] = useState<any>(null);
   const token = useAuthStore((state) => state.token); // get token from Zustand
-  //const authUser = useAuthStore((state) => state.user); // Firebase user object
+  const [data, setData] = useState<UserScoreInterface>();
 
   useEffect(() => {
-    if (!token || !uid) return console.log("Missing Player UID or Token");
-  }, []);
+    if (!token) return console.log("Missing Player Token");
+    const fetchData = async () => {
+      try {
+        const fetchedUser = await getUserScores(token);
+        setData(fetchedUser);
+        console.log(fetchedUser);
+      } catch (error) {
+        console.error("Failed to fetch user score data:", error);
+      }
+    };
+    fetchData();
+  }, [token]);
+
+  const name = data ? `${data.firstName} ${data.lastName}` : "";
 
   return (
     <>
-      <Layout title={"Player"}>
-        <h2>Player ID: {uid}</h2>
+      <Layout title={name}>
+        <h2>Welcome back </h2>
+        {!data ? (
+          <p>No scores yet.</p>
+        ) : (
+          <ul>
+            {data.scores.map((score) => (
+              <li key={score.id}>
+                <strong>{score.game.title}</strong> – {score.durationMinutes}{" "}
+                minutes
+                <br />
+                Started: {new Date(score.startedAt).toLocaleString()}
+                <br />
+                Stopped: {new Date(score.stoppedAt).toLocaleString()}
+              </li>
+            ))}
+          </ul>
+        )}
+        );
       </Layout>
     </>
   );
