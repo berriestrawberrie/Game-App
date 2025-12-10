cfebe7a5eec4726a21ed9e0a8ee4b67b25ac16a8
berriestrawberrie <brooke.henriksson@outlook.com>
2025-12-10 16:17:32 +0100

    Added scores table to game page, next step post the player score

--- frontend/src/routes/PlayGame.tsx
+++ frontend/src/routes/PlayGame.tsx
@@ -1,9 +1,19 @@
 import Layout from "../components/Layout";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 import Timer from "../components/Timer";
 import HelpBox from "../components/GameRules";
+import { getGameScores } from "../api/scoreHandler";
+import type { ScoreInterface } from "../interfaces/interfaces";
+
+interface ScoreData extends ScoreInterface {
+  id: number;
+  user: {
+    firstName: string;
+    lastName: string;
+  };
+}
 
 const PlayGame = () => {
   const { gameTitle, gameId } = useParams<{
@@ -11,11 +21,33 @@ const PlayGame = () => {
     gameId: string;
   }>();
   const numericGameId: number = Number(gameId);
+  const [data, setData] = useState<ScoreData[]>();
+  const [loading, setLoading] = useState(true);
   const user = useAuthStore((state) => state.user);
   const token = useAuthStore((state) => state.token);
+  const userName = localStorage.getItem("name");
+  const avatar = localStorage.getItem("avatar");
+  useEffect(() => {
+    if (!token) {
+      console.log("Missing Player Token");
+      setLoading(false);
+      return;
+    }
 
-  useEffect(() => console.log(numericGameId, user, token), []);
+    const fetchData = async () => {
+      try {
+        const fetchedScores = await getGameScores(token, numericGameId);
+        setData(fetchedScores);
+        console.log(fetchedScores);
+      } catch (error) {
+        console.error("Failed to fetch Scores data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchData();
+  }, [token]);
   return (
     <>
       <Layout title={gameTitle!}>
@@ -32,6 +64,40 @@ const PlayGame = () => {
           )}
         </div>
         <Timer />
+        <div className="flex flex-col-reverse items-center justify-center mt-4 sm:flex-row">
+          {/*TABLE */}
+          <table className="text-center mx-auto mt-3 bg-light-100 border-separate  border-spacing-2 rounded-lg dark:bg-dark-100">
+            <thead>
+              <tr>
+                <th>GameID</th>
+                <th>Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data &&
+                data.map((score, index) => (
+                  <tr key={index}>
+                    <td>{score.gameId}</td>
+                    <td>{`${score.user.firstName}`}</td>
+                    <td>{score.durationMinutes ?? "-"}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          {/*USER DISPLAY */}
+          <div className="text-center text-xl">
+            <img
+              className="mx-auto h-[300px]"
+              src={avatar ?? "/user-2.png"}
+              alt={`${userName} avatar`}
+              onError={(e) => {
+                e.currentTarget.src = "/user-2.png";
+              }}
+            />
+            {userName}
+          </div>
+        </div>
       </Layout>
     </>
   );
--- frontend/src/routes/SelectGames.tsx
+++ frontend/src/routes/SelectGames.tsx
@@ -4,17 +4,24 @@ import { useAuthStore } from "../store/authStore";
 import { getAllGames } from "../api/gameHandler";
 import type { GameInterface } from "../interfaces/interfaces";
 import { Link } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-export interface GameData extends GameInterface {
+interface GameData extends GameInterface {
   id: number;
 }
 
 const SelectGame = () => {
   const token = useAuthStore((state) => state.token);
   const [data, setData] = useState<GameData[]>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!token) return console.log("Missing Player Token");
+    if (!token) {
+      console.log("Missing Player Token");
+      setLoading(false);
+      return;
+    }
 
     const fetchData = async () => {
       try {
@@ -23,6 +30,8 @@ const SelectGame = () => {
         console.log(fetchedGames);
       } catch (error) {
         console.error("Failed to fetch games data:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -31,36 +40,47 @@ const SelectGame = () => {
   return (
     <>
       <Layout title="Select A Game">
-        <div className="flex flex-wrap gap-2  justify-center">
-          {data &&
-            data.map((game) => (
-              <Link
-                key={game.id}
-                to={`/playgame/${game.title}/${game.id}`}
-                title={game.title}
-              >
-                <div
+        {loading ? (
+          // Loader centered on the page
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="50vh"
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <div className="flex flex-wrap gap-2 justify-center">
+            {data &&
+              data.map((game) => (
+                <Link
                   key={game.id}
-                  className="rounded-xl bg-light-200 w-[200px] h-[250px] p-2
-            dark:bg-dark-100 dark:text-white"
+                  to={`/playgame/${game.title}/${game.id}`}
+                  title={game.title}
                 >
-                  {game.id === 1 ? (
-                    <i className="fa-solid text-6xl text-center fa-chess-knight w-full my-2"></i>
-                  ) : game.id === 2 ? (
-                    <i className="fa-solid text-6xl text-center fa-dice w-full my-2"></i>
-                  ) : game.id === 3 ? (
-                    <i className="fa-solid text-6xl text-center fa-puzzle-piece w-full my-2"></i>
-                  ) : (
-                    <i className="fa-solid text-6xl text-center fa-table-tennis-paddle-ball w-full my-2"></i>
-                  )}
-                  <h3 className="text-center font-bold text-lg">
-                    {game.title}
-                  </h3>
-                  <p className="m-2">{game.description}</p>
-                </div>
-              </Link>
-            ))}
-        </div>
+                  <div
+                    className="rounded-xl bg-light-200 w-[200px] h-[250px] p-2
+                  dark:bg-dark-100 dark:text-white"
+                  >
+                    {game.id === 1 ? (
+                      <i className="fa-solid text-6xl text-center fa-chess-knight w-full my-2"></i>
+                    ) : game.id === 2 ? (
+                      <i className="fa-solid text-6xl text-center fa-dice w-full my-2"></i>
+                    ) : game.id === 3 ? (
+                      <i className="fa-solid text-6xl text-center fa-puzzle-piece w-full my-2"></i>
+                    ) : (
+                      <i className="fa-solid text-6xl text-center fa-table-tennis-paddle-ball w-full my-2"></i>
+                    )}
+                    <h3 className="text-center font-bold text-lg">
+                      {game.title}
+                    </h3>
+                    <p className="m-2">{game.description}</p>
+                  </div>
+                </Link>
+              ))}
+          </div>
+        )}
       </Layout>
     </>
   );
