8c603a77e1d7afd2ad2009abbcdfc6667217fcbb
berriestrawberrie <brooke.henriksson@outlook.com>
2025-12-15 14:06:14 +0100

    Changed port to 10000

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use("/players", authenticateToken, usersRoute);
 app.use("/games", authenticateToken, gamesRoute);
 app.use("/scores", authenticateToken, scoresRoute);
 
-const PORT = process.env.PORT || 4000;
+const PORT = 10000;
 app.listen(PORT, () =>
   console.log(`Backend running on port this is updated: ${PORT}`)
 );
