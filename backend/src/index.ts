6ffefe1517f33e323e268aac2da8a86475815468
berriestrawberrie <brooke.henriksson@outlook.com>
2025-12-15 11:10:04 +0100

    Reconfigured backend to run with render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,15 +9,20 @@ const app = express();
 // Allow requests from your frontend origin
 app.use(
   cors({
-    origin: "http://localhost:5173", // React dev server
+    origin: ["http://localhost:5173", "https://gameapp-frontend.onrender.com/"], // React dev server & prod
     credentials: true, // if you send cookies/auth headers
   })
 );
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.use("/players", authenticateToken, usersRoute);
 app.use("/games", authenticateToken, gamesRoute);
 app.use("/scores", authenticateToken, scoresRoute);
 
-app.listen(4000, () => console.log("Backend running on port 4000"));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Backend running on port 4000"));
